1273baed50008d17c451b32c1a3a158a9b90fd45
Artem Bubnov <bf-man@mail.ru>
2020-07-31 09:18:56 +0300

    Implement binary

--- src/search/binary/index.js
+++ src/search/binary/index.js
@@ -1,3 +1,11 @@
-export function binarySearch(array, item) {
-  return item;
+export default function binarySearch(array, item) {
+  return search(array, item, 0, array.length);
+}
+
+function search(array, item, startIndex, endIndex) {
+  const middle = Math.floor((startIndex + endIndex) / 2);
+  if (array[middle] === item) return middle;
+  if (startIndex >= endIndex) return null;
+  if (array[middle] > item) return search(array, item, startIndex, middle - 1);
+  return search(array, item, middle + 1, endIndex);
 }
--- src/search/binary/index.test.js
+++ src/search/binary/index.test.js
@@ -1,5 +1,29 @@
-import { binarySearch } from './index';
+import binarySearch from './index';
 
-test('Searches', () => {
-  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).toBe(3);
+it('Searches', () => {
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).toBe(2);
+});
+
+it('Searches at low boundary', () => {
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)).toBe(0);
+});
+
+it('Searches at high boundary', () => {
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)).toBe(10);
+});
+
+it('Searches in low middle', () => {
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 5)).toBe(5);
+});
+
+it('Searches in high middle', () => {
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 6)).toBe(6);
+});
+
+it('Searches in the middle', () => {
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)).toBe(5);
+});
+
+it('Returns null if not found', () => {
+  expect(binarySearch([0, 1, 3, 4, 5, 6, 7, 8, 9, 10], 2)).toBe(null);
 });
